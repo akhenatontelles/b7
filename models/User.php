379235c1cf2777506def85e4a2d10e40e599d779
Akhenaton Telles <akhenatontelles@hotmail.com>
2021-12-21 13:45:21 -0300

    DEVE1

---
+++ models/User.php
@@ -0,0 +1,18 @@
+<?php
+class User{
+    public  $id;
+    public $email;
+    public $password;
+    public $name;
+    public $birthdate;
+    public $city;
+    public $work;
+    public $avatar;
+    public $token;
+}
+
+interface UserDAO{
+    public function findByToken($token);
+    public function findByEmail($email);
+    public function update(User $u);
+}
